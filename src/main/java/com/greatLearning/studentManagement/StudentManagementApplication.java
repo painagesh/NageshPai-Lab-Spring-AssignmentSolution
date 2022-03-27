2543e6483bff4adf4e81cf367f612a2bf893a6ce
painagesh <riverjazmine@gmail.com>
2022-03-27 17:55:30 +0530

    Lab 6 Spring Boot

---
+++ src/main/java/com/greatLearning/studentManagement/StudentManagementApplication.java
@@ -0,0 +1,22 @@
+package com.greatLearning.studentManagement;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan(basePackages= {"com.greatLearning.studentManagement"})
+public class StudentManagementApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello Spring Boot");
+		
+	}
+
+}
